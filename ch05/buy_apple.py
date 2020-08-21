8e6479cde4db53451288001a0d3f769b82e138b3
Aoi <ao.oz.725@gmail.com>
2020-08-21 15:58:27 +0900

    様子を確認

--- ch05/buy_apple.py
+++ ch05/buy_apple.py
@@ -6,8 +6,7 @@ apple = 100
 apple_num = 2
 tax = 1.1
 
-mul_apple_layer = MulLayer()
-mul_tax_layer = MulLayer()
+mul_apple_layer, mul_tax_layer = MulLayer(), MulLayer()
 
 # forward
 apple_price = mul_apple_layer.forward(apple, apple_num)
